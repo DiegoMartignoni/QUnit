56ea14cf036f685faf880c982d261ef54dabfc73
Diego Martignoni <diego.martignoni@disc.it>
2018-09-05 16:28:31 +0200

    eseguito test con qunit

---
+++ test/js-object-literal-pattern.js
@@ -0,0 +1,14 @@
+var macchina1 = {
+  anno: 2000,
+  marca: 'Ford',
+  modello: 'Fusion',
+  getInfo: function (){
+    return 'Veicolo: ' + this.anno + ' ' + this.marca + ' ' + this.modello;
+  }
+}
+
+QUnit.test( "Object Literal Test", function( Oggettotest ) {
+  var expected = 'Veicolo: 2000 Ford Fusion';
+  var actual = macchina1.getInfo();
+  Oggettotest.equal(actual, expected, 'Valore aspettato: ' + expected + 'Valore vero: ' + actual);
+});
